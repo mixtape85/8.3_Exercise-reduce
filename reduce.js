20c477a52c74853405705a2201f101ee835cfc92
Mandy Woodbury <85962983+mixtape85@users.noreply.github.com>
2021-09-06 16:34:51 -0400

    add logic for addKeyAndValue function

--- reduce.js
+++ reduce.js
@@ -54,7 +54,12 @@ Examples:
        ]
 */
 
-function addKeyAndValue(arr, key, value) {}
+function addKeyAndValue(arr, key, value) {
+    return arr.reduce((acc,next,i) => {
+        acc[i][key] = value;
+        return acc;
+    },arr);
+}
 
 /*
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
