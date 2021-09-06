9053752f66e29b120704594d173765bce66a47d1
Mandy Woodbury <85962983+mixtape85@users.noreply.github.com>
2021-09-06 16:26:33 -0400

    add logic for vowelCount function

--- reduce.js
+++ reduce.js
@@ -24,7 +24,20 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
+function vowelCount(str) {
+    const vowels = 'aeiou';
+    return str.split('').reduce((acc,next) => {
+        let lower = next.toLowerCase();
+        if(vowels.indexOf(lower) !== -1){
+            if(acc[lower]){
+                acc[lower]++;
+            } else {
+                acc[lower] = 1;
+            }
+        }
+        return acc;
+    },{});
+}
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
